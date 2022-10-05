f158e1eb4183a1699afa2ee2d4eccf38035cf355
DingDeuhTop <DingDeuhTop>
2022-10-05 22:34:39 +0530

    install vueuse motion for animation css in vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,6 @@
 import "./bootstrap";
 import "../css/app.css";
+import { MotionPlugin } from '@vueuse/motion'
 
 import { createApp, h } from "vue";
 import { createInertiaApp } from "@inertiajs/inertia-vue3";
@@ -21,6 +22,7 @@ createInertiaApp({
         return createApp({ render: () => h(app, props) })
             .use(plugin)
             .use(ZiggyVue, Ziggy)
+            .use(MotionPlugin)
             .mount(el);
     },
 });
